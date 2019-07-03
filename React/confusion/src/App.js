9c3269ae67a8fed170dba914a41c1798180c4e91
Serhii Diukarev <serhiidiukarev@Serhiis-MacBook-Pro.local>
2019-07-03 11:00:07 +0300

    refactoring

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-// eslint-disable-next-line
-import logo from './logo.svg';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-// todo install eslint
 function App() {
   return (
-      // eslint-disable-next-line
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
   );
 }
 
