d657ed11ec0fd59b6454f11bcd5683b4d716195e
DiukarevSergii <diukarev.sergii@gmail.com>
2019-07-09 17:51:19 +0300

    improve with using DishDetailComponent

--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import {
-  Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle,
+  Card, CardImg, CardImgOverlay, CardTitle,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
   static propTypes = {
@@ -22,20 +23,8 @@ class Menu extends Component {
   }
 
   renderDish(dish) {
-    if (dish != null) {
-      const { image, name, description } = dish;
-      return (
-        <Card>
-          <CardImg top src={image} alt={name} />
-          <CardBody>
-            <CardTitle>{name}</CardTitle>
-            <CardText>{description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    }
     return (
-      <div />
+      <DishDetail dish={dish} />
     );
   }
 
@@ -44,10 +33,10 @@ class Menu extends Component {
     const { selectedDish } = this.state;
 
     const menu = dishes.map(dish => (
-      // todo refactor to flex box
+      // todo: After finish course refactor to css grid
       <div key={dish.id} className="col-12 col-md-5 m-1">
         <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
-          <CardImg width="10%" src={dish.image} alt={dish.name} />
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
           <CardImgOverlay>
             <CardTitle>{dish.name}</CardTitle>
           </CardImgOverlay>
@@ -59,8 +48,6 @@ class Menu extends Component {
       <div className="container">
         <div className="row">
           {menu}
-        </div>
-        <div className="col-12 col-md-5 m-1">
           {this.renderDish(selectedDish)}
         </div>
       </div>
