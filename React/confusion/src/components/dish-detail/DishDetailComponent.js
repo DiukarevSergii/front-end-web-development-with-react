460281556d2d301965a5ba119813974048828d21
DiukarevSergii <diukarev.sergii@gmail.com>
2019-07-18 10:11:39 +0300

    Functional Components

--- React/confusion/src/components/dish-detail/DishDetailComponent.js
+++ React/confusion/src/components/dish-detail/DishDetailComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Card, CardImg, CardText, CardBody, CardTitle,
 } from 'reactstrap';
@@ -7,90 +7,104 @@ import { Row, Col } from 'react-bootstrap';
 import { isEmpty } from 'lodash';
 import './DishDetailComponent.scss';
 
+function RenderComments({ comments = [] }) {
+  if (comments.length > 0) {
+    const selectedDishComments = comments.map((comment) => {
+      const {
+        id, author, data, sentence,
+      } = comment;
+
+      return (
+        <Row key={id}>
+          <CardText>{sentence}</CardText>
+          <CardText>{`-- ${author}, ${data}`}</CardText>
+          <CardText> </CardText>
+          <CardText> </CardText>
+        </Row>
+      );
+    });
+
+    return (
+      <Col xs={6} md={6}>
+        <Row className="item">
+          <h4>Comments</h4>
+        </Row>
+        <Row className="item">
+          {selectedDishComments}
+        </Row>
+      </Col>
+    );
+  }
+  return '';
+}
+
+RenderComments.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
 /**
- * DishDetail.
- *
- * @param {Object} props - Component props.
+ * @type {propTypes}
+ * @param {Object} dish - The selected dish
  */
-class DishDetail extends Component {
-    /**
-     * @type {propTypes}
-     * @param {Object} dish - The selected dish
-     */
-    static propTypes = {
-      dish: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
-    };
+function RenderDish({ dish }) {
+  const { image, name, description } = dish;
+  return (
+    <Col xs={6} md={6} key={dish.id}>
+      <Card className="item">
+        <CardImg src={image} alt={name} />
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+          <CardText>{description}</CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+}
 
-    /**
-     * @type {defaultProps}
-     * @prop {object} - default dish value.
-     */
-    static defaultProps = {
-      dish: {},
-    };
+RenderDish.propTypes = {
+  dish: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+};
 
-    renderComments(comments = []) {
-      if (comments.length > 0) {
-        const selectedDishComments = comments.map((comment) => {
-          const {
-            id, author, data, sentence,
-          } = comment;
+/**
+ * @type {defaultProps}
+ * @prop {object} - default dish value.
+ */
+RenderDish.defaultProps = {
+  dish: {},
+};
 
-          return (
-            <Row key={id}>
-              <CardText>{sentence}</CardText>
-              <CardText>{`-- ${author}, ${data}`}</CardText>
-              <CardText> </CardText>
-              <CardText> </CardText>
-            </Row>
-          );
-        });
-
-        return (
-          <Col xs={6} md={6}>
-            <Row className="item">
-              <h4>Comments</h4>
-            </Row>
-            <Row className="item">
-              {selectedDishComments}
-            </Row>
-          </Col>
-        );
-      }
-      return '';
-    }
-
-    /**
+/**
      * Render function.
      *
      * @returns {Object} A dom element for an element.
      */
-    render() {
-      const { dish } = this.props;
+const DishDetail = (props) => {
+  const { dish } = props;
 
-      if (!isEmpty(dish)) {
-        const {
-          image, name, description, comments,
-        } = dish;
-        return (
-          <Row className="show-item">
-            <Col xs={6} md={6} key={dish.id}>
-              <Card className="item">
-                <CardImg src={image} alt={name} />
-                <CardBody>
-                  <CardTitle>{name}</CardTitle>
-                  <CardText>{description}</CardText>
-                </CardBody>
-              </Card>
-            </Col>
-            {this.renderComments(comments)}
-          </Row>
-        );
-      }
-      return (
-        <div />
-      );
-    }
-}
+  if (!isEmpty(dish)) {
+    const { comments } = dish;
+    return (
+      <Row className="show-item">
+        <RenderDish dish={dish} />
+        <RenderComments comments={comments} />
+      </Row>
+    );
+  }
+  return (
+    <div />
+  );
+};
+
+DishDetail.propTypes = {
+  dish: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+};
+
+/**
+ * @type {defaultProps}
+ * @prop {object} - default dish value.
+ */
+DishDetail.defaultProps = {
+  dish: {},
+};
 
 export default DishDetail;
--- React/confusion/src/components/menu/MenuComponent.js
+++ React/confusion/src/components/menu/MenuComponent.js
@@ -1,36 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Card, CardImg, CardImgOverlay, CardTitle,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-bootstrap';
 
-class Menu extends Component {
-  static propTypes = {
-    dishes: PropTypes.arrayOf(PropTypes.object).isRequired,
-    onClick: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { dishes, onClick } = this.props;
-
-    const menu = dishes.map(dish => (
-      <Col xs={6} md={6} key={dish.id}>
-        <Card className="item" key={dish.id} onClick={() => onClick(dish.id)}>
-          <CardImg src={dish.image} alt={dish.name} />
-          <CardImgOverlay>
-            <CardTitle>{dish.name}</CardTitle>
-          </CardImgOverlay>
-        </Card>
-      </Col>
-    ));
-
-    return (
-      <Row className="show-item">
-        {menu}
-      </Row>
-    );
-  }
+function RenderMenuItem({ dish, onClick }) {
+  return (
+    <Col xs={6} md={6} key={dish.id}>
+      <Card className="item" key={dish.id} onClick={() => onClick(dish.id)}>
+        <CardImg src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+    </Col>
+  );
 }
+
+RenderMenuItem.propTypes = {
+  dish: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+const Menu = (props) => {
+  const { dishes, onClick } = props;
+
+  const menu = dishes.map(dish => (
+    <RenderMenuItem dish={dish} onClick={onClick} />
+  ));
+
+  return (
+    <Row className="show-item">
+      {menu}
+    </Row>
+  );
+};
+
+Menu.propTypes = {
+  dishes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
 
 export default Menu;
