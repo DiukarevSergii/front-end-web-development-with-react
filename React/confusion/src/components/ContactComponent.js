3671c510842ee7fc1e03e0df8f7ec7864e08b55e
DiukarevSergii <diukarev.sergii@gmail.com>
2019-08-05 09:30:19 +0300

    fix eslint problem

--- React/confusion/src/components/ContactComponent.js
+++ React/confusion/src/components/ContactComponent.js
@@ -12,17 +12,17 @@ class Contact extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(values) {
+  handleSubmit = (values) => {
     console.log(`Current State is: ${JSON.stringify(values)}`);
     alert(`Current State is: ${JSON.stringify(values)}`);
-  }
+  };
 
 
   render() {
     const required = val => val && val.length;
     const maxLength = len => val => !(val) || (val.length <= len);
     const minLength = len => val => val && (val.length >= len);
-    const isNumber = val => !isNaN(Number(val));
+    const isNumber = val => !Number.isNaN(Number(val));
     const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
     return (
