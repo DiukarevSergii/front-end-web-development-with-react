ccf80f7aeb1f922af9bd4be098e67080f7a2b983
DiukarevSergii <diukarev.sergii@gmail.com>
2019-08-26 06:14:38 +0300

    extend action

--- React/confusion/src/redux/promotions.js
+++ React/confusion/src/redux/promotions.js
@@ -1,7 +1,25 @@
-import { PROMOTIONS } from '../shared/promotions';
+import * as ActionTypes from './ActionTypes';
 
-export const Promotions = (state = PROMOTIONS, action) => { // eslint-disable-line
+// eslint-disable-next-line import/prefer-default-export
+export const Promotions = (state = {
+  isLoading: true,
+  errMess: null,
+  promosList: [],
+}, action) => {
   switch (action.type) {
+    case ActionTypes.ADD_PROMOS:
+      return {
+        ...state, isLoading: false, errMess: null, dishesList: action.payload,
+      };
+
+    case ActionTypes.PROMOS_LOADING:
+      return {
+        ...state, isLoading: true, errMess: null, dishesList: [],
+      };
+
+    case ActionTypes.PROMOS_FAILED:
+      return { ...state, isLoading: false, errMess: action.payload };
+
     default:
       return state;
   }
